02c736b8e77a6510144e251441f03c85b23ff6de
Gyeol <ohhunyoung@gmail.com>
2021-12-15 23:50:23 +0900

    12강 완료

--- src/bubblebubble/test/ex02/BackgroundPlayerService.java
+++ src/bubblebubble/test/ex02/BackgroundPlayerService.java
@@ -27,16 +27,19 @@ public class BackgroundPlayerService implements Runnable{
 			// 색상 확인
 			Color leftColor = new Color(image.getRGB(player.getX() - 1, player.getY() + 25));
 			Color rightColor = new Color(image.getRGB(player.getX() + 50 + 15, player.getY() + 25));
-			Color color = new Color(image.getRGB(player.getX(), player.getY()));
-			//System.out.println("leftColor : " +color);
-			//System.out.println("rightColor : " +color);
+			int bottomColor = image.getRGB(player.getX() + 25, player.getY() + 50 + 5)
+					+ image.getRGB(player.getX() + 35, player.getY() + 50 + 5);
 			
+			// 바닥 충돌 확인
+			if(bottomColor != -2) {
+				player.setDown(false);
+			}
+			
+			// 외벽 충돌 확인
 			if(leftColor.getRed() == 255 && leftColor.getGreen() == 0 && leftColor.getBlue() == 0) {
-				System.out.println("왼쪽 벽에 충돌함");
 				player.setLeftWallCrash(true);
 				player.setLeft(false);
 			}else if(rightColor.getRed() == 255 && rightColor.getGreen() == 0 && rightColor.getBlue() == 0) {
-				System.out.println("오른쪽 벽에 충돌함");
 				player.setRightWallCrash(true);
 				player.setRight(false);
 			}else {
